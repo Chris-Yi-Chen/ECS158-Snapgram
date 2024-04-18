16200d6f2d650bdd290573c0bf76e5be9f3d1f08
Christopher Chen <christopher.yi.chen@gmail.com>
2024-04-17 18:05:15 -0700

    optiimized

--- sg_serial.c
+++ sg_serial.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 
+
 void sg_recommender(uint32_t *G, size_t V, uint32_t *R)
 {
 	uint32_t sum;
@@ -11,28 +12,36 @@ void sg_recommender(uint32_t *G, size_t V, uint32_t *R)
 	memset(C, 0, V * V * sizeof(uint32_t));
 
 	for (size_t i = 0; i < V; i++) {
+        uint32_t max = 0;
+		size_t maxIndex = i;
 		for (size_t j = 0; j < V; j++) {
 			sum = 0;
 			for (size_t k = 0; k < V; k++) {
 				sum += G[i * V + k] * G[k * V + j];
 			}
             C[i * V + j] = sum;
+            if (j != i && G[i * V + j] == 0 && max < sum) {
+				max = sum;
+				maxIndex = j;
+                printf("max: %d\n", max);
+			}
+            R[i] = maxIndex;
 		}
 	}
 
-	for (size_t i = 0; i < V; i++) {
-		uint32_t max = 0;
-		size_t maxIndex = i;
-		for (size_t j = 0; j < V; j++) {
-            if (j == i) {
-                continue;
-            }
-			if (G[i * V + j] == 0 && max < C[i * V + j]) {
-				max = C[i * V + j];
-				maxIndex = j;
-                printf("max: %d\n", max);
-			} 
-		}
-		R[i] = maxIndex;
-	}
+	// for (size_t i = 0; i < V; i++) {
+	// 	uint32_t max = 0;
+	// 	size_t maxIndex = i;
+	// 	for (size_t j = 0; j < V; j++) {
+    //         if (j == i) {
+    //             continue;
+    //         }
+	// 		if (G[i * V + j] == 0 && max < C[i * V + j]) {
+	// 			max = C[i * V + j];
+	// 			maxIndex = j;
+    //             printf("max: %d\n", max);
+	// 		} 
+	// 	}
+	// 	R[i] = maxIndex;
+	// }
 }
