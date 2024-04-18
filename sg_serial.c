3f5c0e37bcbd8961584e3916069d2eb1362b8a9b
Christopher Chen <christopher.yi.chen@gmail.com>
2024-04-17 17:33:52 -0700

    Working

--- sg_serial.c
+++ sg_serial.c
@@ -1,10 +1,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 void sg_recommender(uint32_t *G, size_t V, uint32_t *R)
 {
-	int sum;
+	uint32_t sum;
     uint32_t *C;
     C = aligned_alloc(64, V * V * sizeof(uint32_t));
 	memset(C, 0, V * V * sizeof(uint32_t));
@@ -21,11 +22,15 @@ void sg_recommender(uint32_t *G, size_t V, uint32_t *R)
 
 	for (size_t i = 0; i < V; i++) {
 		uint32_t max = 0;
-		size_t maxIndex = -1;
-		for (size_t j = 0; j < V && j != i; j++) {
-			if (G[i * V + j] != 0 && max < C[i * V + j]) {
+		size_t maxIndex = i;
+		for (size_t j = 0; j < V; j++) {
+            if (j == i) {
+                continue;
+            }
+			if (G[i * V + j] == 0 && max < C[i * V + j]) {
 				max = C[i * V + j];
 				maxIndex = j;
+                printf("max: %d\n", max);
 			} 
 		}
 		R[i] = maxIndex;
